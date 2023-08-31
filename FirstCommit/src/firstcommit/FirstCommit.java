543f4bf87ac8035bbba0d02f66370ddb4169ceaa
Vishnu Vardhan Ramvath <s566760@nwmissouri.edu>
2023-08-31 16:17:17 -0500

    Added new File

---
+++ FirstCommit/src/firstcommit/FirstCommit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstcommit;
+
+/**
+ *
+ * @author S566760
+ */
+public class FirstCommit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");   
+    }
+    
+}
